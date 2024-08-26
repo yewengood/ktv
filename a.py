31a610b2714118a589159fdab65694ea01a48795
yewengood <152235769+yewengood@users.noreply.github.com>
2024-08-26 15:35:37 +0800

    Update a.py

--- a.py
+++ a.py
@@ -237,11 +237,11 @@ with open("itvlist.m3u", 'a', encoding='utf-8') as file:
                 if channel_counters[channel_name] >= result_counter:
                     continue
                 else:
-                    file.write(f"#EXTINF:-1 group-title=\"卫视频道\",{channel_name}\n")
+                    file.write(f"#EXTINF:-1 tvg-logo=https://live.fanmingming.com/tv/{channel_name}.png, group-title=\"卫视频道\",{channel_name}\n")
                     file.write(f"{channel_url}\n")
                     channel_counters[channel_name] += 1
             else:
-                file.write(f"#EXTINF:-1 group-title=\"卫视频道\",{channel_name}\n")
+                file.write(f"#EXTINF:-1 tvg-logo=https://live.fanmingming.com/tv/{channel_name}.png, group-title=\"卫视频道\",{channel_name}\n")
                 file.write(f"{channel_url}\n")
                 channel_counters[channel_name] = 1
     channel_counters = {}
@@ -253,11 +253,11 @@ with open("itvlist.m3u", 'a', encoding='utf-8') as file:
                 if channel_counters[channel_name] >= result_counter:
                     continue
                 else:
-                    file.write(f"#EXTINF:-1 group-title=\"其他频道\",{channel_name}\n")
+                    file.write(f"#EXTINF:-1 tvg-logo=https://live.fanmingming.com/tv/{channel_name}.png, group-title=\"其他频道\",{channel_name}\n")
                     file.write(f"{channel_url}\n")
                     channel_counters[channel_name] += 1
             else:
-                file.write(f"#EXTINF:-1 group-title=\"其他频道\",{channel_name}\n")
+                file.write(f"#EXTINF:-1 tvg-logo=https://live.fanmingming.com/tv/{channel_name}.png, group-title=\"其他频道\",{channel_name}\n")
                 file.write(f"{channel_url}\n")
                 channel_counters[channel_name] = 1                     
                         
